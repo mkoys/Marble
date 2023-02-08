f924e8f342da1d6ce996252da4da5dec3d37fe58
mkoys <mkoys@protonmail.com>
2023-02-08 15:30:04 +0100

    Entrypoint redirect

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = post.app();
 
 app.use(post.static("public"));
 
+app.get("/", (req, res) => res.redirect("/index.html"));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
