0b0fd72eeb1394db786966db9fd6aaf1e5d3beb1
mkoys <mkoys@protonmail.com>
2023-02-14 10:41:52 +0100

    Auto next input in attendance

--- public/components/repa/attendance/attendance.js
+++ public/components/repa/attendance/attendance.js
@@ -7,4 +7,85 @@ export default class RepaAttendance extends BaseComponent {
         this.addStyle("attendance.css", import.meta.url);
         this.useTemplate("/components/repa/attendance/attendance.html");
     }
+
+    load = () => {
+        let box = this.shadowRoot.querySelector(".box");
+        let inputsElement = this.shadowRoot.querySelector(".boxInput");
+        let inputsElementEmpty = inputsElement.cloneNode(true);
+        let previous = null;
+
+        let description = inputsElement.querySelector(".description");
+        let time = inputsElement.querySelector(".time");
+        let classType = inputsElement.querySelector(".class");
+
+        description.addEventListener("input", () => checkNext());
+        time.addEventListener("input", () => checkNext());
+        classType.addEventListener("input", () => checkNext());
+
+        function checkNext() {
+            if (description.value || time.value || classType.value) {
+                if (previous) {
+                    previous.description.removeEventListener("input", () => checkBefore())
+                    previous.time.removeEventListener("input", () => checkBefore())
+                    previous.classType.removeEventListener("input", () => checkBefore())
+                }
+
+                description.removeEventListener("input", () => checkNext());
+                time.removeEventListener("input", () => checkNext());
+                classType.removeEventListener("input", () => checkNext());
+
+                previous = { description, time, classType, inputsElement };
+
+                previous.description.addEventListener("input", () => checkBefore())
+                previous.time.addEventListener("input", () => checkBefore())
+                previous.classType.addEventListener("input", () => checkBefore())
+
+                const inputsElementCopy = inputsElementEmpty.cloneNode(true);
+                inputsElement = inputsElementCopy;
+                description = inputsElement.querySelector(".description");
+                time = inputsElement.querySelector(".time");
+                classType = inputsElement.querySelector(".class");
+
+                description.addEventListener("input", () => checkNext());
+                time.addEventListener("input", () => checkNext());
+                classType.addEventListener("input", () => checkNext());
+                box.insertBefore(inputsElement, box.children[box.childElementCount - 1]);
+            }
+        }
+
+        function checkBefore() {
+            if (previous && !description.value && !time.value && !classType.value && !previous.description.value && !previous.time.value && !previous.classType.value) {
+                box.removeChild(box.children[box.childElementCount - 2]);
+                const before = box.children[box.childElementCount - 3];
+
+                previous.description.removeEventListener("input", () => checkBefore())
+                previous.time.removeEventListener("input", () => checkBefore())
+                previous.classType.removeEventListener("input", () => checkBefore())
+                const current = previous;
+
+                if (before.classList.contains("boxInput")) {
+                    const description = before.querySelector(".description");
+                    const time = before.querySelector(".time");
+                    const classType = before.querySelector(".class");
+
+                    previous = { description, time, classType, inputsElement: before }
+
+                    previous.description.addEventListener("input", () => checkBefore())
+                    previous.time.addEventListener("input", () => checkBefore())
+                    previous.classType.addEventListener("input", () => checkBefore())
+                }else {
+                    previous = null;
+                }
+
+                inputsElement = current.inputsElement;
+                description = inputsElement.querySelector(".description");
+                time = inputsElement.querySelector(".time");
+                classType = inputsElement.querySelector(".class");
+
+                description.addEventListener("input", () => checkNext());
+                time.addEventListener("input", () => checkNext());
+                classType.addEventListener("input", () => checkNext());
+            }
+        }
+    }
 }
