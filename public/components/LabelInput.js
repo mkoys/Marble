5de0d928d8ba99e5644ac62a11727602013e3d3e
mkoys <mkoys@protonmail.com>
2023-03-05 13:37:04 +0100

    Messages to inputs

--- public/components/LabelInput.js
+++ public/components/LabelInput.js
@@ -2,7 +2,7 @@ import BaseComponent from "../source/BaseComponent.js";
 
 export default class LabelInput extends BaseComponent {
 
-    static get observedAttributes() { return ["label", "type"] }
+    static get observedAttributes() { return ["label", "type", "message"] }
 
     constructor() {
         super();
@@ -13,14 +13,19 @@ export default class LabelInput extends BaseComponent {
 
     async attributeChangedCallback(name, oldValue, newValue) {
         await this.load;
+
         switch (name) {
             case "label":
                 const labelElement = this.shadowRoot.querySelector("label");
                 labelElement.textContent = newValue;
                 break;
-            case "type":
+                case "type":
                 const inputElement = this.shadowRoot.querySelector("input");
                 inputElement.setAttribute("type", newValue);
+                break;
+            case "message":
+                const messageElement = this.shadowRoot.querySelector("p");
+                messageElement.textContent = newValue ? "- " + newValue : null;
                 break;
 
             default:
